4f50ec9c494269992e3e5ff8bcd9754a90699049
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-09-25 16:29:55 -0400

    Adds resource builders to seeds file for all

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,32 @@ tests = [
   ["Total Tannin", nil, "%"]
 ]
 
+tests.each do |t|
+  Test.create!(type: t[0], abbreviation: t[1], unit: t[2])
+end
+
 # Stages have a description and a location
 stages = [
-  ["pre-fermentation", "cellar"],
-  ["fermentation", "cellar"],
+  ["pre-fermentation", "Cellar"],
+  ["fermentation", "Cellar"],
   ["storage", "Production Room"],
   ["pre-packaging", "Cold Room"],
   ["post-packaging", "Cellar"]
 ]
 
+stages.each do |stage|
+  Stage.create!(description: stage[0], location: stage[1], unit: stage[2])
+end
+
 # Users have a name, email, password
 users = [
   ["Ethan F.", "efertsch@gmail.com", "password"]
 ]
+
+users.each do |user|
+  User.create!(name: user[0], email: user[1], password_digest: user[2])
+end
+
 
 # Pitches have a yeast_type, lees?, round
 pitches = [
@@ -37,17 +50,30 @@ pitches = [
   ["VL-3", false, 0]
 ]
 
+pitches.each do |pitch|
+  Pitch.create!(yeast_type: pitch[0], lees?: pitch[1], round: pitch[2])
+end
+
 # Batches have a date_received and apple_composition
 batches = [
   [Date.new(2018, 9, 1), "macintosh"],
   [Date.new(2018, 11, 1), "macintosh, cortland"]
 ]
 
+batches.each do |batch|
+  Batch.create!(date_received: batch[0], apple_composition: batch[1])
+end
+
 # Groups have a name
 groups = [
   ["SG-1"],
   ["SG-2"]
 ]
+
+groups.each do |group|
+  Group.create!(name: group[0])
+end
+
 
 # Samples have a start_date, finish_date, tote_size, batch_id, group_id, stage_id, pitch_id
 samples = [
@@ -62,6 +88,10 @@ samples = [
   [Date.new(2018, 11, 1), 275, 2, 2, 1, 1]
 ]
 
+samples.each do |sample|
+  Sample.create!(start_date: sample[0], finish_date: sample[1], tote_size: sample[2], batch_id: sample[3], group_id: sample[4], stage_id: sample[5], pitch_id: sample[6])
+end
+
 # Additions have a chemical_name, amount, notes, unit, date, sample_id
 additions = [
   ["PMS", "260" "g", Date.new(2018, 9, 1), "Initial SO2 dose", 1],
@@ -70,6 +100,10 @@ additions = [
   ["PMS", "260" "g", Date.new(2018, 9, 1), "Initial SO2 dose", 4]
 ]
 
+additions.each do |addition|
+  Addition.create!(chemical_name: addition[0], amount: addition[1], unit: addition[2], date: addition[3], notes: addition[4], sample_id: addition[5])
+end
+
 # Experiments have a date, value, test_id, sample_id, notes
 experiments = [
   [Date.new(2018, 9, 1), 4.0, 2, 1, "Higher than usual pH"],
@@ -77,6 +111,10 @@ experiments = [
   [Date.new(2018, 9, 1), 3.8, 2, 3, "Higher than usual pH"],
   [Date.new(2018, 9, 1), 3.6, 2, 4, "Higher than usual pH"]
 ]
+
+experiments.each do |experiment|
+  Experiment.create!(date: experiment[0], value: experiment[1], test_id: experiment[2], sample_id: experiment[3], notes: experiment[4])
+end
 
 # Appointments have a date, test_id, sample_id, description
 appointments = [
@@ -89,3 +127,7 @@ appointments = [
   [Date.new(2018, 11, 28), 1, 6, "Incoming juice pH"],
   [Date.new(2018, 12, 5), 1, 7, "Incoming juice pH"]
 ]
+
+appointments.each do |appointment|
+  Appointment.create!(date: appointment[0], test_id: appointment[1], sample_id: appointment[2], description: appointment[3])
+end
