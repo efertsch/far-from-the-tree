b55d5e73beb4da610807a80b271d6a1f9380ef9d
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-02 16:41:14 -0400

    Sets up user auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url, notice: 'Thank you for registering!'
@@ -16,6 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password_digest, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
