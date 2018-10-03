c6992590436d4915272ca6c33c44429a60987a88
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-03 13:56:48 -0400

    Adds new sample form to corresponding view

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -5,4 +5,23 @@ class SamplesController < ApplicationController
     @samples = Sample.all.order(:id)
   end
 
+  def new
+    @sample = Sample.new
+  end
+
+  def create
+    @sample = Sample.new
+    if sample.save
+      redirect_to samples_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sample_params
+    params.require(:sample).permit(:start_date, :tote_size, :yeast_type, :pitch_round, :batch_id, :group_id)
+  end
+
 end
