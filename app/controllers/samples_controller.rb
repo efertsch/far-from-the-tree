bae64f35902abb6a96bdf756ecb9beb4997b6903
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-10-03 15:47:01 -0400

    Finishes create action for sample

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -10,18 +10,22 @@ class SamplesController < ApplicationController
   end
 
   def create
-    @sample = Sample.new
-    if sample.save
+    @sample = Sample.new(sample_params)
+    if @sample.save
       redirect_to samples_path
     else
       render :new
     end
   end
 
+  def show
+    @sample = Sample.find(params[:id])
+  end
+
   private
 
   def sample_params
-    params.require(:sample).permit(:start_date, :tote_size, :yeast_type, :pitch_round, :batch_id, :group_id)
+    params.require(:sample).permit(:start_date, :tote_size, :yeast_type, :round, :juice_shipment_id, :stage_id, :group)
   end
 
 end
